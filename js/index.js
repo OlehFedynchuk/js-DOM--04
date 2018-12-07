7109a9f9a9302b37d66a2c08fc01ad7f911d97d0
Oleg Fedynchuk <oleh.fedynchuk.tk.2015@lpnu.ua>
2018-12-07 17:59:21 +0000

    commit dfnfd

--- js/index.js
+++ js/index.js
@@ -1,48 +1,31 @@
-document.querySelectorAll('.digits, .operation, .division')
-    .forEach(el => el.addEventListener('click', digitOperClick));
+(function() {
+    document.querySelectorAll('.operation, .digits, .division ').
+    forEach(el => el.addEventListener('click', xxx))
 
-function digitOperClick(e) {
-    const target = e.target;
-    const display = document.querySelector('.display');
-    display.value += target.innerText;
-}
-document.querySelector('.equal')
-    .addEventListener('click', equal);
+    function xxx(el) {
+        const target = el.target;
+        const display = document.querySelector('.display');
+        display.value += target.innerText;
+    }
+    document.querySelector('.equal').
+    addEventListener('click', equal);
 
-function equal() {
-    const display = document.querySelector('.display');
-    display.value = eval(display.value);
-}
-
-document.querySelector('.clear')
-    .addEventListener('click', reset);
-
-function reset() {
-    const display = document.querySelector('.display');
-    display.value = "";
-}
-
-document.querySelector('.square')
-    .addEventListener('click', square);
-
-function square() {
-    const display = document.querySelector('.display');
-    display.value *= display.value;
-}
-
-document.querySelector('.division')
-    .addEventListener('click', division);
-
-function division() {
-    const display = document.querySelector('.display');
-    display.value.divisionN;
-}
-function divisionN () {
-    const display = document.querySelector('.display');
-   if (display.value > 0) {
+    function equal() {
+        const display = document.querySelector('.display');
         display.value = eval(display.value);
-   }else {
-       display.value += display.value.innerText('ksd');
-   }
-    
-}
+    }
+    document.querySelector('.clear').
+    addEventListener('click', clear);
+
+    function clear() {
+        const display = document.querySelector('.display');
+        display.value = "";
+    }
+    document.querySelector('.square').
+    addEventListener('click', square);
+
+    function square() {
+        const display = document.querySelector('.display');
+        display.value *= display.value;
+    }
+})();
